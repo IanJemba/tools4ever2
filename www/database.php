d1d858064a5a14fb4271a07e20ce9fea00f9e034
IanJemba <ianjemba38@gmail.com>
2024-02-08 10:56:43 +0100

    Cloned the project and updated the database using PDO

--- www/database.php
+++ www/database.php
@@ -1,15 +1,15 @@
 <?php
 
 //database connection   
-$dbhost = "mariadb";
-$dbuser = "root";
-$dbpass = "password";
-$dbname = "tools4ever";
+$servername = "mariadb";
+$username = "root";
+$password = "password";
 
-$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
-
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=tools4ever", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
 }
-
